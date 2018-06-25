8c322ca93cd1802a51b67db2593dab3191b64371
Ted Leary and William Chu <ted+william@>
2018-06-25 11:25:29 -0700

    looping in jsx, populating multiple components within render function

--- src/components/Suggestion.js
+++ src/components/Suggestion.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 
-function Suggestion(){
+function Suggestion(props){
   return(
     <div className='suggestion-wrapper'>
       <div className='suggestion-image'></div>
       <div className='suggestion-content'>
-        <h3>Place Holder</h3>
+        <h3>{props.name}</h3>
         <div className='suggestion-button'><p>Button</p></div>
       </div>
     </div>
--- src/components/SuggestionList.js
+++ src/components/SuggestionList.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
 import Suggestion from './Suggestion.js'
 
+const suggestionArray = [
+  {
+    name: 'Brad',
+    description: 'An ok friend.'
+  },
+  {
+    name: 'Dave',
+    description: 'Friend of the century.'
+  },
+  {
+    name: 'Brendan',
+    description: 'A jealous and terrible friend.'
+  }
+];
+
 function SuggestionList(){
   return(
     <div className='suggestionlist-wrapper'>
-      <h3>Suggestion List</h3>
-      <Suggestion/>
+      <div className='suggestionlist-top'>
+        <h3>Suggestion List</h3>
+      </div>
+
+      {suggestionArray.map((suggestion) =>
+        <Suggestion
+          name={suggestion.name}
+          description={suggestion.description}/>
+      )}
+
     </div>
   );
 }
